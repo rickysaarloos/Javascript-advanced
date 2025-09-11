0e533699212a8f6ea83749d8b8b119df32b5b1a5
rickysaarloos <9027376@student.zadkine.nl>
2025-09-11 11:52:49 +0200

    Opdracht 1: Basis Array Methods

--- 1-js-methods/level-1-basis-methods/1.2-map-oefening/app.js
+++ 1-js-methods/level-1-basis-methods/1.2-map-oefening/app.js
@@ -5,7 +5,7 @@ const namen = ['sanne', 'johan', 'sjoerd'];
 console.log('Originele namen:', namen);
 
 // TODO: Zet alle namen om naar hoofdletter
-const hoofdletterNamen = /* jouw code hier */
+const hoofdletterNamen = namen.map(naam => naam.charAt(0).toUpperCase() + naam.slice(1)); /* jouw code hier */
 
 console.log('Namen met hoofdletter:', hoofdletterNamen);
 console.log('Verwacht resultaat: [\'Sanne\', \'Johan\', \'Sjoerd\']');
