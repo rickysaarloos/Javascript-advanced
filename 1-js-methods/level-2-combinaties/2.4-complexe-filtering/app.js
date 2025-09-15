a47bae62dbae94763bfbf9963e8a13dea57f2e2c
rickysaarloos <9027376@student.zadkine.nl>
2025-09-15 13:46:44 +0200

    Opdracht 2: Combinatie van Array Methods

--- 1-js-methods/level-2-combinaties/2.4-complexe-filtering/app.js
+++ 1-js-methods/level-2-combinaties/2.4-complexe-filtering/app.js
@@ -8,14 +8,26 @@ console.log('Alle getallen:', getallen);
 console.log('Alle woorden:', woorden);
 
 // TODO: Filter getallen die groter zijn dan 10 EN kleiner dan 20
-const middelsteGetallen = /* jouw code hier */
+const middelsteGetallen = getallen.filter(checkGetallen);
+
+function checkGetallen(getal){
+    return getal >= 10 && getal <= 20;
+}
 console.log('Getallen tussen 10 en 20:', middelsteGetallen);
 
 // TODO: Filter woorden die beginnen met 'a' EN langer zijn dan 5 letters
-const langeAwoorden = /* jouw code hier */
+const langeAwoorden = woorden.filter(checkLangeWoorden)
+
+function checkLangeWoorden(langWoord){
+    return langWoord[0] === "a" && langWoord.length > 5;
+
+}
+
 console.log('Lange woorden met a:', langeAwoorden);
 
 // TODO: Filter even getallen die groter zijn dan 15
-const groteEvenGetallen = /* jouw code hier */
+const groteEvenGetallen = getallen.filter(getal => getal % 2 ===0 && getal >=15 );
+
+
 console.log('Grote even getallen:', groteEvenGetallen);
 
